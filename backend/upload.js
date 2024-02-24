f34bfe9b165c410c4537515752ca0f4545b2e020
githubinfovikas <vikasdbg453@gmail.com>
2024-02-24 12:35:32 +0530

    change

--- backend/upload.js
+++ backend/upload.js
@@ -26,46 +26,18 @@
 
 
 
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
-
-
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./certificate/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-module.exports = upload;
-
-
-
-
-
-
 // const express = require('express');
 // const multer = require('multer');
 // const fs = require('fs');
 // const path = require('path');
 
-// // Get the absolute path of the directory containing the current script
-// const scriptDirectory = path.dirname(require.main.filename);
+
 
 // const storage = multer.diskStorage({
 //   destination: function (req, file, cb) {
-//     // Concatenate the script directory with the relative path to the destination
-//     const destinationPath = path.join(scriptDirectory, "certificate");
-//     cb(null, destinationPath);
+//     cb(null, "./certificate/");
 //   },
-//   filename: function (req, file, cb){
+//   filename: function (req, file, cb) {
 //     cb(null, file.originalname);
 //   },
 // });
@@ -73,3 +45,31 @@ module.exports = upload;
 // const upload = multer({ storage: storage });
 
 // module.exports = upload;
+
+
+
+
+
+
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+// Get the absolute path of the directory containing the current script
+const scriptDirectory = path.dirname(require.main.filename);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Concatenate the script directory with the relative path to the destination
+    const destinationPath = path.join(scriptDirectory, "certificate");
+    cb(null, destinationPath);
+  },
+  filename: function (req, file, cb){
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
